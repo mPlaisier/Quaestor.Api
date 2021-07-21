edfba8480734fe2ff06dc830bb82f5665a425ace
Mattias Plaisier <mattias.plaisier@flowpilots.com>
2021-07-21 14:57:41 +0200

    #8 remove unused

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -3,7 +3,6 @@ const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
 const { accountService } = require('../services');
-const logger = require('../config/logger');
 
 const createAccount = catchAsync(async (req, res) => {
   const account = await accountService.createAccount({ ...req.body, user: req.user });
